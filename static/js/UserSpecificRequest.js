6dbd5eab44181896362c900376758a47cdfcebca
Ruth Waiganjo <ruthwaiganjo@gmail.com>
2018-06-26 01:21:44 +0300

    [ft #158424686]Implemented delete functionality

--- static/js/UserSpecificRequest.js
+++ static/js/UserSpecificRequest.js
@@ -19,11 +19,11 @@ let request_id = window.localStorage.getItem('request_id')
 	})
 
 // user edit specific request
-let put_request = document.getElementById('edit_request')
+let put_request = document.getElementById('edit_request');
 put_request.addEventListener('submit', editrequest);
 
 
-// user can create requests
+// user can modify requests
 
 function editrequest(e) {
 
@@ -59,8 +59,42 @@ function editrequest(e) {
 
             if(data.response === "request modified successfuly"){
                 document.getElementById('output').style.color = 'green'
+                document.getElementById('output').innerHTML = data.response;
+            }
+            }
+        })
+}
+
+// user can delete requests
+let button = document.getElementById("delete");
+button.addEventListener("click", delete_request);
+
+function delete_request(e){
+
+    e.preventDefault();
+    let request_id = window.localStorage.getItem('request_id');
+
+
+    fetch('https://young-depths-42728.herokuapp.com/api/v2/users/requests/'+ request_id, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-type': 'application/json',
+                'token': window.localStorage.getItem('token')
+            }
+        })
+
+        .then((res) => res.json())
+        .then((data) => {
+        	console.log(data)
+            if (data.response != undefined) {
+                document.getElementById('output').style.color = 'red'
                 document.getElementById('output').innerHTML = data.response
-                document.getElementById("create_request").reset();
+
+            if(data.response === "the record has been successfuly deleted"){
+                document.getElementById('output').style.color = 'green'
+                document.getElementById('output').innerHTML = data.response
+                document.getElementById("edit_request").reset();
             }
             }
         })
